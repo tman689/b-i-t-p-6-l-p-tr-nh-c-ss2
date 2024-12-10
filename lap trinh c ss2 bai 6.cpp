aa49d2902df4aa200012898bd383cfa1512b48dd
tman689 <tranminhan05092000@gmail.com>
2024-12-10 21:16:41 +0700

    hoàn thnhaf bài tạp 6

---
+++ lap trinh c ss2 bai 6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	const float pi = 3.14;
+	float bk = 4.0;
+	
+	float cv = 2 * pi * bk;
+	float dt = pi * bk * bk;
+	
+	printf("Chu vi hinh tron: %.2f\n", cv);
+	printf("dien tich hinh tron: %.2f\n", dt);
+	
+	return 0;
+		
+}
+	
+	 
+	
+	
+		
+		
